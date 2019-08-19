e2bc317e2da26ec89ee360a2055a22bf26bf43bb
bb_domgarcia <dom.garcia@nuworks.ph>
2019-08-19 20:53:23 +0800

    updatess

--- .storybook/stories/input.stories.js
+++ .storybook/stories/input.stories.js
@@ -2,6 +2,8 @@ import React, {Fragment} from 'react';
 import { storiesOf } from '@storybook/react';
 import {Input, InputGroup} from '../../src/@components';
 import '../styles/global.css';
+import '../styles/mini-grid-system.css';
+import '../styles/custom.css';
 
 storiesOf('Input Text', module)
     // settings
@@ -11,29 +13,82 @@ storiesOf('Input Text', module)
         <Input placeholder="Input your name" />
     ))
     .add('with property align', () => (
-        <Input align="right" value="Domz Garcia" />
+        <Input align="right" defaultValue="Domz Garcia" />
     ))
     .add('with custom class', () => (
-        <Input className="red bordered" align="center" value="Domz Garcia" />
+        <Input className="red bordered" align="center" defaultValue="Domz Garcia" />
     ))
     .add('with property block', () => (
-        <Input block value="Domz Garcia" />
+        <Input block defaultValue="Domz Garcia" />
+    ))
+    .add('with label', () => (
+        <div className="container -story-frame">
+            <div className="row">
+                <div className="col">
+                    <label className="nw-label">Hello</label>
+                </div>
+                <div className="col">
+                    <Input block defaultValue="Domz Garcia" />
+                </div>
+            </div>
+        </div>
+    ), {info: "[NOTICE] Use any Grid system you like to align label and input. The example provided above use Bootstrap Grid." })
+
+storiesOf('Addon', module)
+    .add('with addon before', () => (
+        <Input placeholder="Email address"
+            addOnBefore={<div role="img">
+                <span >😊</span>
+            </div>}
+        />
+    ))
+    .add('with addon after', () => (
+        <Input placeholder="Email address"
+            addOnAfter={<div>
+                <span role="img">😊</span>
+            </div>}
+        />
+    ))
+    .add('with addon before, after and block', () => (
+        <div>
+            <Input placeholder="First Name" block
+                addOnBefore={<div>
+                    <span role="img">😊</span>
+                </div>}
+            />
+            <br />
+            <Input placeholder="Family Name" block
+                addOnAfter={<div role="img">
+                    <span>😊</span>
+                </div>}
+            />
+        </div>
     ))
 
 storiesOf('InputGroup', module)
-    .addDecorator(children => <InputGroup compact>{children()}</InputGroup>)
-    .add('with property compact', () => {
+    // .addDecorator(children => <InputGroup compact>{children()}</InputGroup>)
+    .add('none compact', () => {
         return (
         <div>
-            <Input value="Domz Garcia" />
-            <Input value="Domz Garcia" />
+            <Input defaultValue="Domz Garcia" />&nbsp;&nbsp;
+            <Input defaultValue="Domz Garcia" />
         </div>)
     })
-    .add('with property compact 3 elements', () => {
+    .add('with property compact', () => {
         return (
-        <div>
-            <Input value="Domz Garcia" />
-            <Input value="Domz Garcia" />
-            <Input value="Domz Garcia" />
-        </div>)
+            <InputGroup compact>  
+                <Input defaultValue="Domz Garcia" />
+                <Input defaultValue="Domz Garcia" />
+            </InputGroup>
+        )
+    })
+    .add('with property compact on more than 2 elements', () => {
+        return (
+        <InputGroup compact>
+            <Input defaultValue="John " />
+            <Input defaultValue="Dello" />
+            <Input defaultValue="Smith" />
+            <Input defaultValue="Bella" />
+            <Input defaultValue="Chou" />
+        </InputGroup>)
     })
--- src/@components/Input/Input.js
+++ src/@components/Input/Input.js
@@ -2,25 +2,74 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './input.scss';
 
-const Input = (props) => (
-	<input {...props} type="text" className={generateClassName(props)} />
-);
+const Input = (props) => {
+	const {html, settings} = resolveAddOn(props);
+	const extendedProps = Object.assign({}, props);
+	extendedProps.settings = settings;
+	
+	delete extendedProps.addOnBefore;
+	delete extendedProps.addOnAfter;
+	
+	return(
+		<span className={`input-wrap ${resolveBlock(props.block)}`}>
+			{html}
+			<input {...extendedProps} type="text"
+			className={generateClassName(extendedProps)} />
+		</span>
+	)
+};
 
-const generateClassName = ({ className, block, align }) => {
+const resolveBlock = (isBlock) => {
+	const emptyClass = '';
+	if(isBlock) return '-block';
+	return emptyClass;
+}
+
+const generateClassName = ({ className, align, settings}) => {
+	
 	const nativeClasses = (className) ? className : '';
 	let ownClasses = ['nw-input', 
 	...alignment(align),
-	...isBlock(block),
+	...addOnClasses(settings),
 	];
 	ownClasses = ownClasses.join(" ");
 	return `${ownClasses} ${nativeClasses}`;
 };
 
-const isBlock = (block) => {
-	if(block) return ['block'];
-	return [];
+const resolveAddOn = ({addOnBefore, addOnAfter}) => {
+	let html = <React.Fragment/>;
+	const exists 	= (addOnBefore) ? true : (addOnAfter) ? true : false;
+	const position 	= (addOnBefore) ? 'before' : (addOnAfter) ? 'after' : '';
+	let children 	= (addOnBefore) 
+					? addOnBefore.props.children
+					: (addOnAfter) 
+						? addOnAfter.props.children 
+						: <React.Fragment />;
+
+	if(addOnBefore || addOnAfter){
+		html = (
+		<div className={`nw-addon -${position}`}>
+			{children}
+		</div>)
+	}
+	return {
+		html: html,
+		settings: {
+			exists,
+			position,
+		}
+	};
 }
 
+/////////////////
+// Add Classes
+/////////////////
+const addOnClasses = (settings) => {
+	if(settings.exists) {
+		return [`-${settings.position}-adjust-padding`];
+	}
+	return [];
+}
 const alignment = (align) => {
 	if(align) return [`align-${align}`];
 	return [];
@@ -29,6 +78,8 @@ const alignment = (align) => {
 Input.propTypes = {
 	align: PropTypes.string,
 	block: PropTypes.bool,
+	addOnBefore: PropTypes.object,
+	addOnAfter:  PropTypes.object,
 }
 
 export default Input;
